93002cd44a5bf78e4efc3ed0b8a222a54719afe3
ElectricVampire <34921378+ElectricVampire@users.noreply.github.com>
2024-10-05 22:05:52 +0530

    Show query even if query plan times out (#38)

--- src/IQueryableObjectSource/EFCoreQueryableObjectSource.cs
+++ src/IQueryableObjectSource/EFCoreQueryableObjectSource.cs
@@ -6,6 +6,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Diagnostics;
 
 namespace IQueryableObjectSource
 {
@@ -22,54 +26,122 @@ namespace IQueryableObjectSource
 
             try
             {
-                using var command = queryable.CreateDbCommand();
-                var provider = GetDatabaseProvider(command);
-
-                if (provider == null)
+                var operationType = GetOperationType(incomingData);
+                switch (operationType)
                 {
-                    return;
+                    case OperationType.GetQuery:
+                        HandleGetQuery(queryable, outgoingData);
+                        break;
+
+                    case OperationType.NotSupported:
+                        throw new InvalidOperationException("Unknown operation type."); 
+
+                    default:
+                        HandleGetQueryPlan(queryable, incomingData, outgoingData);
+                        break;
                 }
-
-                var query = queryable.ToQueryString();
-                var rawPlan = provider.ExtractPlan();
-
-                var buffer = new byte[3];
-                var isBackgroundDarkColor = false;
-
-                var r = 255;
-                var g = 255;
-                var b = 255;
-
-                if (incomingData.Read(buffer, 0, buffer.Length) == buffer.Length)
-                {
-                    r = buffer[0];
-                    g = buffer[1];
-                    b = buffer[2];
-                }
-
-                isBackgroundDarkColor = r * 0.2126 + g * 0.7152 + b * 0.0722 < 255 / 2.0;
-
-                var planFile = Path.Combine(provider.GetPlanDirectory(ResourcesLocation), Path.ChangeExtension(Path.GetRandomFileName(), "html"));
-
-                var planPageHtml = File.ReadAllText(Path.Combine(provider.GetPlanDirectory(ResourcesLocation), "template.html"))
-                    .Replace("{backColor}", $"rgb({r} {g} {b})")
-                    .Replace("{textColor}", isBackgroundDarkColor ? "white" : "black")
-                    .Replace("{plan}", JavaScriptEncoder.UnsafeRelaxedJsonEscaping.Encode(rawPlan).Replace("'", "\\'"))
-                    .Replace("{query}", JavaScriptEncoder.UnsafeRelaxedJsonEscaping.Encode(query).Replace("'", "\\'"));
-
-                File.WriteAllText(planFile, planPageHtml);
-
-                using var writer = new BinaryWriter(outgoingData, Encoding.Default, true);
-                writer.Write(false);
-                writer.Write(planFile);
             }
             catch (Exception ex)
             {
-                using var writer = new BinaryWriter(outgoingData, Encoding.Default, true);
-                writer.Write(true);
-                writer.Write(ex.Message);
+                WriteError(outgoingData, ex.Message);
             }
         }
+        private void HandleGetQuery(IQueryable queryable, Stream outgoingData)
+        {
+            using var queryWriter = new BinaryWriter(outgoingData, Encoding.Default, true);
+            queryWriter.Write(false); // Indicates no error
+            queryWriter.Write(GenerateHtml(queryable.ToQueryString()));
+        }
+        private void HandleGetQueryPlan(IQueryable queryable, Stream incomingData, Stream outgoingData)
+        {
+            using var command = queryable.CreateDbCommand();
+            var provider = GetDatabaseProvider(command);
+
+            if (provider == null)
+            {
+                return;
+            }
+
+            var query = queryable.ToQueryString();
+            var rawPlan = provider.ExtractPlan();
+
+            var (r, g, b) = ReadBackgroundColor(incomingData);
+            var isBackgroundDarkColor = r * 0.2126 + g * 0.7152 + b * 0.0722 < 255 / 2.0; 
+
+            var planFile = GeneratePlanFile(provider, query, rawPlan, r, g, b, isBackgroundDarkColor);
+
+            using var writer = new BinaryWriter(outgoingData, Encoding.Default, true);
+            writer.Write(false); // Indicates no error
+            writer.Write(planFile);
+        }
+        private (int r, int g, int b) ReadBackgroundColor(Stream incomingData)
+        {
+            var buffer = new byte[3];
+            var r = 255;
+            var g = 255;
+            var b = 255;
+
+            if (incomingData.Read(buffer, 0, buffer.Length) == buffer.Length)
+            {
+                r = buffer[0];
+                g = buffer[1];
+                b = buffer[2];
+            }
+
+            return (r, g, b);
+        }
+
+        private string GeneratePlanFile(DatabaseProvider provider, string query, string rawPlan, int r, int g, int b, bool isBackgroundDarkColor)
+        {
+            var planDirectory = provider.GetPlanDirectory(ResourcesLocation);
+            var planFile = Path.Combine(planDirectory, Path.ChangeExtension(Path.GetRandomFileName(), "html"));
+
+            var planPageHtml = File.ReadAllText(Path.Combine(planDirectory, "template.html"))
+                .Replace("{backColor}", $"rgb({r} {g} {b})")
+                .Replace("{textColor}", isBackgroundDarkColor ? "white" : "black")
+                .Replace("{plan}", JavaScriptEncoder.UnsafeRelaxedJsonEscaping.Encode(rawPlan).Replace("'", "\\'"))
+                .Replace("{query}", JavaScriptEncoder.UnsafeRelaxedJsonEscaping.Encode(query).Replace("'", "\\'"));
+
+            File.WriteAllText(planFile, planPageHtml);
+
+            return planFile;
+        }
+
+        private void WriteError(Stream outgoingData, string errorMessage)
+        {
+            using var writer = new BinaryWriter(outgoingData, Encoding.Default, true);
+            writer.Write(true); // Indicates an error occurred
+            writer.Write(errorMessage);
+        }
+
+        public static OperationType GetOperationType(Stream stream)
+        {
+            try
+            {
+                var operationBuffer = new byte[1];
+                stream.Read(operationBuffer, 0, 1);
+                return (OperationType)operationBuffer[0];
+            }
+            catch (Exception)
+            {
+                return OperationType.NotSupported;
+            }
+        }
+
+        private static string GenerateHtml(string query)
+        {
+            string escapedQuery = WebUtility.HtmlEncode(query);
+            string templatePath = Path.Combine(ResourcesLocation,"Common", "template.html");
+            if (!File.Exists(templatePath))
+            {
+                throw new FileNotFoundException("Common Query template file not found", templatePath);
+            }
+
+            string templateContent = File.ReadAllText(templatePath);
+            string finalHtml = templateContent.Replace("{query}", escapedQuery);
+            return finalHtml;
+        }
+
 
         private static DatabaseProvider GetDatabaseProvider(DbCommand command)
         {
